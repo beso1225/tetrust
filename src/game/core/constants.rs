3408e3ce05d6e38839fb85495503f2e7e98ccbcc
beso1225 <yutaroformisc@gmail.com>
2025-12-15 23:18:24 +0900

    assign each block's color

--- src/game/core/constants.rs
+++ src/game/core/constants.rs
@@ -15,3 +15,12 @@ pub const Z_OFFSETS: [IVec2; 4] = [IVec2::new(0, 0), IVec2::new(-1, 0), IVec2::n
 
 pub const INITIAL_SPAWN_GRID_Y: i32 = GRID_HEIGHT as i32 - 3;
 pub const INITIAL_SPAWN_GRID_X: i32 = (GRID_WITH / 2) as i32 - 1;
+
+// definitions of block colors
+pub const T_COLOR: Color = Color::srgb(0.6, 0.0, 0.6);
+pub const I_COLOR: Color = Color::srgb(0.0, 0.6, 0.6);
+pub const O_COLOR: Color = Color::srgb(0.6, 0.6, 0.0);
+pub const L_COLOR: Color = Color::srgb(0.6, 0.3, 0.2);
+pub const J_COLOR: Color = Color::srgb(0.2, 0.3, 0.6);
+pub const S_COLOR: Color = Color::srgb(0.2, 0.6, 0.2);
+pub const Z_COLOR: Color = Color::srgb(0.6, 0.2, 0.2);
--- src/game/system/spawn/spawn_block.rs
+++ src/game/system/spawn/spawn_block.rs
@@ -13,6 +13,15 @@ pub fn spawn_block(shape: BlockShape, mut commands: Commands, mut grid: ResMut<G
         }
     }
     let translation = grid.grid_to_world(pos.x, pos.y);
+    let color = match shape {
+        BlockShape::T => T_COLOR,
+        BlockShape::I => I_COLOR,
+        BlockShape::O => O_COLOR,
+        BlockShape::L => L_COLOR,
+        BlockShape::J => J_COLOR,
+        BlockShape::S => S_COLOR,
+        BlockShape::Z => Z_COLOR,
+    };
 
     let block_entity = commands.spawn((
         Transform::from_translation(translation),
@@ -31,7 +40,7 @@ pub fn spawn_block(shape: BlockShape, mut commands: Commands, mut grid: ResMut<G
         );
         commands.spawn((
             Sprite {
-                color: Color::srgb(0.6, 0.2, 0.8),
+                color,
                 custom_size: Some(Vec2::splat(CELL_SIZE)),
                 ..default()
             },
@@ -39,83 +48,5 @@ pub fn spawn_block(shape: BlockShape, mut commands: Commands, mut grid: ResMut<G
             ChildOf(block_entity),
         ));
         grid.set_entity(cell.x, cell.y, Some(block_entity));
-    }
-}
-
-pub fn spawn_l_block(mut commands: Commands, mut grid: ResMut<Grid>) {
-    let shape = BlockShape::L;
-    let pos = IVec2::new(5, 0);
-    let translation = grid.grid_to_world(pos.x, pos.y);
-
-    let block_entity = commands.spawn((
-        Transform::from_translation(translation),
-        Visibility::default(),
-        Block,
-        BlockState{ state: BlockStateEnum::Landed, shape },
-        Position { x: pos.x, y: pos.y },
-    )).id();
-
-    for offset in shape.offsets() {
-        let cell = pos + *offset;
-        let local = Vec3::new(
-            (*offset).x as f32 * CELL_SIZE,
-            (*offset).y as f32 * CELL_SIZE,
-            0.0,
-        );
-        commands.spawn((
-            Sprite {
-                color: Color::srgb(0.6, 0.3, 0.2),
-                custom_size: Some(Vec2::splat(CELL_SIZE)),
-                ..default()
-            },
-            Transform::from_translation(local),
-            ChildOf(block_entity),
-        ));
-        grid.set_entity(cell.x, cell.y, Some(block_entity));
-    }
-}
-
-fn spawn_blocks(mut commands: Commands, mut grid: ResMut<Grid>) {
-    let blocks = vec![
-        BlockShape::T,
-        BlockShape::I,
-        BlockShape::O,
-        BlockShape::L,
-        BlockShape::J,
-        BlockShape::S,
-        BlockShape::Z,
-    ];
-
-    let mut pos = IVec2::new(5, 18);
-
-    for shape in &blocks {
-        info!("Block Shape: {:?}", shape);
-        let translation = grid.grid_to_world(pos.x, pos.y);
-        let origin = IVec2::new(GRID_WITH as i32 / 2, GRID_HEIGHT as i32 - 2);
-        let block_entity = commands.spawn((
-            Transform::from_translation(translation),
-            Visibility::default(),
-            Block,
-            BlockState{ state: BlockStateEnum::Falling, shape: *shape },
-            Position { x: pos.x, y: pos.y },
-        )).id();
-
-        for offset in shape.offsets() {
-            info!("Offset: {:?}", offset);
-            let cell = origin + *offset;
-            let translation = grid.grid_to_world(cell.x, cell.y);
-            commands.spawn((
-                Sprite {
-                    color: Color::srgb(0.6, 0.2, 0.8),
-                    custom_size: Some(Vec2::splat(CELL_SIZE)),
-                    ..default()
-                },
-                Transform::from_translation(translation),
-                ChildOf(block_entity),
-                Position { x: cell.x, y: cell.y },
-            ));
-            grid.set_entity(cell.x, cell.y, Some(block_entity));
-        }
-        pos.y -= 3;
     }
 }
